5b9254f1686c4d1404f3d983dab2e54e4762d911
John <ashton.basile@gmail.com>
2023-10-23 16:26:45 +0100

    the rockPaperScissors tests all pass

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,4 +1,34 @@
-function rockPaperScissors(number) {}
+function rockPaperScissors(item1, item2) {
+  switch (item1) {
+    case "rock":
+      switch (item2) {
+        case "paper":
+          return "Player 2 wins!";
+        case "rock":
+          return "It's a tie!";
+        case "scissors":
+          return "Player 1 wins!";
+      }
+    case "paper":
+      switch (item2) {
+        case "paper":
+          return "It's a tie!";
+        case "rock":
+          return "Player 1 wins!";
+        case "scissors":
+          return "Player 2 wins!";
+      }
+    case "scissors":
+      switch (item2) {
+        case "paper":
+          return "Player 1 wins!";
+        case "rock":
+          return "Player 2 wins!";
+        case "scissors":
+          return "It's a tie!";
+      }
+  }
+}
 
 // Export the function using CommonJS
 module.exports = rockPaperScissors;
